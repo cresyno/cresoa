fc56cb81c04f4a3e5aed8406bb647243ecc158e5
cresyno <taiwoabraham640@gmail.com>
2026-07-30 23:46:45 -0400

    Fix import path for staffAuth module

--- app/api/staff/invite/route.js
+++ app/api/staff/invite/route.js
@@ -2,7 +2,7 @@
 
 import { NextResponse } from 'next/server'
 import { supabase } from '../../../../lib/supabaseClient'
-import { isOwner, canAddStaff, getActiveStaffCount } from '../../lib/staffAuth'
+import { isOwner, canAddStaff, getActiveStaffCount } from '../../../../lib/staffAuth'
 import { getStaffLimit } from '../../../../lib/planLimits'
 
 export const dynamic = 'force-dynamic'
