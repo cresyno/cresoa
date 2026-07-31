5e4ef886b12fd1d2a90a0c0fe0130857d865e860
cresyno <taiwoabraham640@gmail.com>
2026-07-31 02:14:09 -0400

    Fix import path for supabaseClient in StaffPage

--- app/dashboard/staff/page.js
+++ app/dashboard/staff/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { supabase } from '../../../../../lib/supabaseClient'
+import { supabase } from '../../../../../../lib/supabaseClient'
 import { isOwner, canPerformAction } from '../../../../../lib/staffAuth'
 
 export default function StaffPage() {
