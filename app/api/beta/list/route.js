d954dbb1944f8e4287728e5e56c91f51df3f5c5c
cresyno <taiwoabraham640@gmail.com>
2026-08-05 07:06:14 -0400

    Fix export statement for dynamic in route.js

--- app/api/beta/list/route.js
+++ app/api/beta/list/route.js
@@ -2,7 +2,8 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic' 
+
 const ADMIN_EMAIL = 'taiwoabraham640@gmail.com'
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
