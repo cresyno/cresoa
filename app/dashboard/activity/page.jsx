55a791b0541f745c495dcec2646f9037a51d3add
cresyno <taiwoabraham640@gmail.com>
2026-08-06 19:09:50 -0400

    Refactor import path for getCurrentBusinessId

--- app/dashboard/activity/page.jsx
+++ app/dashboard/activity/page.jsx
@@ -3,8 +3,7 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { supabase } from '../../../lib/supabaseClient'
-import { supabaseAdmin } from '../../../lib/supabaseAdmin'
-import { getCurrentBusinessId } from '../../lib/getBusinessId'
+import { getCurrentBusinessId } from '../../../lib/getBusinessId'
 
 export default function ActivityPage() {
   const router = useRouter()
