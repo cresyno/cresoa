111349b2a6368cb7895962d32a95da76551e9617
cresyno <taiwoabraham640@gmail.com>
2026-08-12 08:03:11 +0100

    Fix import statement for globals.css

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@
 import { Inter } from 'next/font/google'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
-import './globals.css
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
