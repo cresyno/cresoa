ae44a616d26388988435367c2926e3517043c001
cresyno <taiwoabraham640@gmail.com>
2026-08-15 12:50:51 +0100

    Update CSS import path in EditOrderPage

--- app/dashboard/orders/[id]/edit/page.js
+++ app/dashboard/orders/[id]/edit/page.js
@@ -9,7 +9,7 @@ import { Card } from '../../../../../components/Card'
 import { SectionHeader } from '../../../../../components/SectionHeader'
 import { Navigation } from '../../../../../components/Navigation'
 import { MeasurementForm } from '../../../../../components/MeasurementForm'
-import '../../../../../globals.css'
+import '../../../../globals.css'
 
 export default function EditOrderPage() {
   const router = useRouter()
