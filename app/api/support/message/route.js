ee2bfa00523ba6132250737cc1600faacee59113
cresyno <taiwoabraham640@gmail.com>
2026-08-17 23:02:10 +0100

    Update route.js

--- app/api/support/message/route.js
+++ app/api/support/message/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
-import { createClient } from '../../../lib/supabaseClient';
-import { createClient as createAdminClient } from '../../../lib/supabaseAdmin';
-import { supportEngine } from '../../../lib/support/supportEngine';
+import { createClient } from '../../../../lib/supabaseClient';
+import { createClient as createAdminClient } from '../../../../lib/supabaseAdmin';
+import { supportEngine } from '../../../../lib/support/supportEngine';
 
 export async function POST(req) {
   try {
