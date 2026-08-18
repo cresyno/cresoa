925065dfe124fd090c47345fb38dae85c1592701
cresyno <taiwoabraham640@gmail.com>
2026-08-18 01:29:43 +0100

    Fix import path for SupportContent component
    Corrected the import path for SupportContent to go up two levels.

--- app/support/page.js
+++ app/support/page.js
@@ -3,7 +3,7 @@
 import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 // Corrected import path: going up 2 levels to the components folder
-import SupportContent from '../../../components/support/SupportContent';
+import SupportContent from '../../components/support/SupportContent';
 
 // The default export wraps the content in Suspense
 export default function SupportPage() {
