74da1c1e5567bafc348f47b7c697691bc1508f82
cresyno <taiwoabraham640@gmail.com>
2026-08-19 13:33:24 +0100

    Update import path for ClientLayout component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import ClientLayout from './ClientLayout'
+import ClientLayout from '../ClientLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
