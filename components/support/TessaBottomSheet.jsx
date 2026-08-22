fbe68c5f92d9ed3d0ea6a1be768acf3abca29370
cresyno <taiwoabraham640@gmail.com>
2026-08-22 13:33:06 +0100

    Fix import path for supabaseClient

--- components/support/TessaBottomSheet.jsx
+++ components/support/TessaBottomSheet.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useRef, useEffect } from 'react';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 import ChatMessage from './ChatMessage';
 import ChatInput from './ChatInput';
 
