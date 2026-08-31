b40ae165a6d5af151c87db89de1210e122de9db4
cresyno <taiwoabraham640@gmail.com>
2026-08-31 18:55:56 +0100

    Add PublicPageWrapper component with templates

---
+++ components/public-page/PublicPageWrapper.jsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useState } from 'react'
+import ClassicGold from '../public-templates/ClassicGold'
+import ModernBold from '../public-templates/ModernBold'
+import ElegantMinimal from '../public-templates/ElegantMinimal'
+import FreshSerene from '../public-templates/FreshSerene'
+import DynamicSunrise from '../public-templates/DynamicSunrise'
+import QuoteModal from './QuoteModal'
+import ReviewForm from './ReviewForm'
+
+const TEMPLATES = {
+  'classic-gold': ClassicGold,
+  'modern-bold': ModernBold,
+  'elegant-minimal': ElegantMinimal,
+  'fresh-serene': FreshSerene,
+  'dynamic-sunrise': DynamicSunrise,
+}
+
+export default function PublicPageWrapper({ business, page, services, portfolio, reviews, templateId }) {
+  const [quoteOpen, setQuoteOpen] = useState(false)
+  const [reviewOpen, setReviewOpen] = useState(false)
+
+  const Template = TEMPLATES[templateId] || ClassicGold
+
+  return (
+    <>
+      <Template
+        business={business}
+        page={page}
+        services={services}
+        portfolio={portfolio?.map(url => ({ url })) || []}
+        reviews={reviews}
+        onQuoteClick={() => setQuoteOpen(true)}
+        onReviewClick={() => setReviewOpen(true)}
+      />
+
+      <QuoteModal
+        open={quoteOpen}
+        onClose={() => setQuoteOpen(false)}
+        businessId={page.business_id}
+        businessName={business.name}
+      />
+
+      <ReviewForm
+        open={reviewOpen}
+        onClose={() => setReviewOpen(false)}
+        businessId={page.business_id}
+        businessName={business.name}
+        onSubmitted={() => window.location.reload()}
+      />
+    </>
+  )
+}
