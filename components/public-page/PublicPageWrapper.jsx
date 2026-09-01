91aa3df433b8ed273bce295a0b5600df0ebfbaed
cresyno <taiwoabraham640@gmail.com>
2026-09-01 07:37:19 +0100

    Add 'shop' prop to PublicPageWrapper component

--- components/public-page/PublicPageWrapper.jsx
+++ components/public-page/PublicPageWrapper.jsx
@@ -17,7 +17,7 @@ const TEMPLATES = {
   'dynamic-sunrise': DynamicSunrise,
 }
 
-export default function PublicPageWrapper({ business, page, services, portfolio, reviews, templateId }) {
+export default function PublicPageWrapper({ business, page, services, shop, portfolio, reviews, templateId }) {
   const [quoteOpen, setQuoteOpen] = useState(false)
   const [reviewOpen, setReviewOpen] = useState(false)
 
@@ -29,11 +29,13 @@ export default function PublicPageWrapper({ business, page, services, portfolio,
         business={business}
         page={page}
         services={services || []}
+        shop={shop || []}
         portfolio={portfolio || []}
         reviews={reviews || []}
         onQuoteClick={() => setQuoteOpen(true)}
         onReviewClick={() => setReviewOpen(true)}
       />
+
       <QuoteModal open={quoteOpen} onClose={() => setQuoteOpen(false)} businessId={page.business_id} businessName={business.name} />
       <ReviewForm open={reviewOpen} onClose={() => setReviewOpen(false)} businessId={page.business_id} businessName={business.name} onSubmitted={() => window.location.reload()} />
     </>
